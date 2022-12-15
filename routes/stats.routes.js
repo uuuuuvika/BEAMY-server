2dcf30adecf6ef42036d7028d5c91cb9a6843b24
Vika Shcherbak <vikashcherbaka@gmail.com>
2022-12-15 13:40:56 +0100

    stats

--- routes/stats.routes.js
+++ routes/stats.routes.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
 const mongoose = require("mongoose");
 const Stats = require("../models/Stats.model.js");
+const { isAuthenticated } = require("../middleware/jwt.middleware.js");
+const Deck = require("../models/Deck.model.js");
+const Card = require("../models/Card.model.js");
 
 
 router.post('/:deckId', (req, res) => {
@@ -13,19 +16,42 @@ router.post('/:deckId', (req, res) => {
         .catch((err) => res.json(err));
 });
 
+//
+router.get('/lastStudied', isAuthenticated, (req, res) => {
 
-router.get('/:deckId', (req, res) => {
+    const user = req.payload;
 
-    const user = req.body;
+    Stats.find({ userId: user._id }).sort({ createdAt: -1 }).limit(1)
+        .then((response) => {
+            if (!response) {
+                res.json([]);
+            }
+            else {
+                //console.log("DECK", response[0].deckId)
+                Card.find({ deckId: response[0].deckId })
+                    .populate('deckId')
+                    .then((cards) => {
+                        console.log("DECK", cards)
+                        res.json(cards)
+                    })
+            }
+        })
+        .catch(error => res.json(error));
+});
+
+router.get('/:deckId', isAuthenticated, (req, res) => {
+
     const { deckId } = req.params;
+    const user = req.payload;
 
     Stats.find({ $and: [{ userId: user._id }, { deckId: deckId }] })
         .then((response) => {
             if (!response) {
                 res.json([]);
-            } else {
+            }
+            else {
                 const statArray = response.map((statObject) => ({
-                    percentage: 1.0 - (Math.min(statObject.numClicks - statObject.numCardsInDeck, statObject.numCardsInDeck) / statObject.numCardsInDeck)
+                    percentage: Math.round((1.0 - (Math.min(statObject.numClicks - statObject.numCardsInDeck, statObject.numCardsInDeck) / statObject.numCardsInDeck)) * 100) / 100
                 }))
                 res.json(statArray);
             }
